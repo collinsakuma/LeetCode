a69f68093ac920cac672e0815f35dba5f230e4f6
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-28 16:36:14 -0700

    solution test added

---
+++ Problems/58. Length of Last Word/python/test.py
@@ -0,0 +1,7 @@
+from length_of_last_word import Solution
+
+def test_answer():
+    assert Solution().lengthOfLastWord("Hello World") == 5
+
+def test_answer1():
+    assert Solution().lengthOfLastWord("fly me to the moon") == 4
