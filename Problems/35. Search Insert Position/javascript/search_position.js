64327836e1117ae829cd3209fa52f07838644cd1
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-28 16:20:27 -0700

    solution explanation added

--- Problems/35. Search Insert Position/javascript/search_position.js
+++ Problems/35. Search Insert Position/javascript/search_position.js
@@ -1,9 +1,13 @@
 const searchInsert = (nums, target) => {
     if (nums.includes(target)) {
+        // if nums array has target in it. return the index of the target
         return nums.indexOf(target);
     } else {
+        // if target is not in nums array push the number into the array
         nums.push(target);
+        // sort the array with the target number added
         nums.sort((a,b) => a-b);
+        // return the index of the target in the newly sorted array
         return nums.indexOf(target);
     }
 }
