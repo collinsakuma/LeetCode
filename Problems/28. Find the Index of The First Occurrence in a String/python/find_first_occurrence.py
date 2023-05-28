f2dc2ce9ceda2a83f52f2a8e682dfa5d16686ece
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-28 15:42:05 -0700

    solution explanation added

--- Problems/28. Find the Index of The First Occurrence in a String/python/find_first_occurrence.py
+++ Problems/28. Find the Index of The First Occurrence in a String/python/find_first_occurrence.py
@@ -1,6 +1,8 @@
 class Solution:
     def strStr(self, haystack, needle):
         if needle not in haystack:
+            # if the needle is not in the haystack return -1
             return -1
         else:
             return haystack.find(needle)
+            # return the index where the needle starts in the haystack. 
