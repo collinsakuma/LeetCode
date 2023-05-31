c91eda919a58d518fd3bc5d80d6fcc18a4cf0447
Collin Sakuma <collinmsakuma@gmail.com>
2023-05-31 12:34:22 -0700

    different solution with explanation added

--- Problems/1396. Design Underground System/python/design_underground_sytem.py
+++ Problems/1396. Design Underground System/python/design_underground_sytem.py
@@ -26,3 +26,40 @@ class UndergroundSystem:
         return self.routeTotalTime[routeName] / self.routeCount[routeName]
         # return the totalTime from the dict with key of routeName and divide it by the amount of times that route has been traveled
         # to return the average travel time for that route. 
+
+class UndergroundSystemSolutionTwo:
+    
+    def __init__(self):
+        self.checkedIn = dict() # dict to keep track of passenger checkin information
+        self.timeTrack = dict() # dict to keep track of time spent between stations (travel time), and the number of times a particular route has been traveled.
+
+    def checkIn(self, id, stationName, t):
+        if id not in self.checkedIn:
+        # check if there is already a value for that id. 
+            self.checkedIn[id] = (stationName, t)
+            # create an entry to the checkedIn dict with passanger Id as the key and a tuple with station name and check-in as the value.
+        else:
+            raise ValueError
+        
+    def checkOut(self, id, stationName, t):
+        if id not in self.checkedIn:
+            raise ValueError
+        else:
+            startStation, startTime = self.checkedIn.pop(id)
+            # pops values from the checkedIn dict at the key of id
+            key = (startStation, stationName)
+            # creates a key tuple with the values from checkIn.pop(id)
+            total, count = self.timeTrack.get(key, (0,0))
+            # .get() method used to retrieve the values for the total and count at the key of key(startStation, stationName)
+            # if entry does not exist (no trips between those two specific stations have been made yet) it return a default 
+            # value of (0, 0) assigned to the variables total and count. 
+            self.timeTrack[key] =(total + t - startTime, count + 1)
+            # updates the timeTrack dict by adding to the total duration and incrementing the count by 1 for the key 
+            # representing the trip.  
+        
+    def getAverageTime(self, startStation, endStation):
+        num, den = self.timeTrack.get((startStation, endStation), (0,1))
+        # gets the values of from the dict timeTrack at the key of (startStation, endStation) and sets them to num & den
+        return num / den
+        # return the averageTime to travel between two stations by dividing the total time traveled between the two / by the amount of
+        # times that route has been taken. 
