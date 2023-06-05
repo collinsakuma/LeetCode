e9270998f328721233076b259e5d50454c747205
Collin Sakuma <collinmsakuma@gmail.com>
2023-06-05 13:04:30 -0700

    solution using ques added with explanation

--- Problems/649. Dota2 Senate/python/dota_two_senate.py
+++ Problems/649. Dota2 Senate/python/dota_two_senate.py
@@ -1,4 +1,26 @@
 class Solution:
+    def predictPartyVictory(self, senate):
+        n = len(senate) # get the number of senators before sorting into party
+
+        # create two sorted arrays for Radiant and Dire senators. 
+        radiant_que = [i for i in range(len(senate)) if senate[i] == "R"]
+        dire_que = [j for j in range(len(senate)) if senate[j] == "D"]
+
+        while radiant_que and dire_que:
+            # while both radiant and dire ques are true (exist)
+            radiant = radiant_que.pop(0) # pop out the first index 
+            dire = dire_que.pop(0)
+            if radiant < dire:
+                # if the value poped from the radiant array is greater than the value poped from the dire array
+                # then add a new value to the end of the radiant array.
+                radiant_que.append(n + radiant)
+            else:
+                dire_que.append(n + dire)
+        return "Radiant" if radiant_que else "Dire"
+        # the while loop will stop when either radiant_que or dire_que run out of indexes
+        # return the party that still has values in its array ( the winning party )
+
+
     def predictPartyVictoryNoCheating(self, senate):
     # first solution I came up with passes many test cases but not all
     # logic is too simple and many cases will give wrong output.
