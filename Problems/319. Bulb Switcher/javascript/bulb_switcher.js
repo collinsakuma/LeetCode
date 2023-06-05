92e1051443477632d1f2dbc821991d2db21652bc
Collin Sakuma <collinmsakuma@gmail.com>
2023-06-05 13:56:49 -0700

    javascript solution added with comments

---
+++ Problems/319. Bulb Switcher/javascript/bulb_switcher.js
@@ -0,0 +1,5 @@
+const bulbSwitch = (n) => {
+    return Math.floor(Math.sqrt(n));
+    // Math.sqrt() will return the square root of n
+    // Math.floor will round down the number square rooted number
+}
