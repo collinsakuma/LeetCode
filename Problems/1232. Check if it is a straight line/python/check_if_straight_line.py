da3b24026d4fcf1e190bb77b630f91f846c33496
Collin Sakuma <collinmsakuma@gmail.com>
2023-06-07 10:44:17 -0700

    update explanation

--- Problems/1232. Check if it is a straight line/python/check_if_straight_line.py
+++ Problems/1232. Check if it is a straight line/python/check_if_straight_line.py
@@ -4,6 +4,8 @@ class Solution:
 
         for x3, y3 in coordinates[2:]:
             if (y2 - y1) * (x3 - x1) != (y3 - y1) * (x2 - x1):
+                # checking if the slope is the same
+                # if slope not the same then coordinates do not represent a straight line. 
                 return False
             
         return True
