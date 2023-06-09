ed920502795e944916e8fbf34aed374077cd2f50
Collin Sakuma <collinmsakuma@gmail.com>
2023-06-09 14:03:16 -0700

    solution added with explanation using a dict with letter values

---
+++ Problems/744. Find Smallest Letter Greater than Target/python/smallest_letter_greater_than_target.py
@@ -0,0 +1,39 @@
+class Solution:
+    def nextGreatestLetter(self, letters, target):
+        # create a dict with all letters and associate a number value with each letter.
+        alphabet = {
+            "a":1,
+            "b":2,
+            "c":3,
+            "d":4,
+            "e":5,
+            "f":6,
+            "g":7,
+            "h":8,
+            "i":9,
+            "j":10,
+            "k":11,
+            "l":12,
+            "m":13,
+            "n":14,
+            "o":15,
+            "p":16,
+            "q":17,
+            "r":18,
+            "s":19,
+            "t":20,
+            "u":21,
+            "v":22,
+            "w":23,
+            "x":24,
+            "y":25,
+            "z":26 
+        }
+        
+        for i in letters:
+        # for each letter in the list compare if its values in the alphabet dict is greater than that of the target value.
+        # if the value is greater then the least smallest letter greater than the target has been found
+        # because the letters list is ordered for us there is no need for more complex logic. The first match is the answer
+            if alphabet[i] > alphabet[target]:
+                return i
+        return letters[0]
