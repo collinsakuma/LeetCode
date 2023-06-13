a5e0fcb5964d24a2232ace6e50173ffb15e95e77
Collin Sakuma <collinmsakuma@gmail.com>
2023-06-13 12:16:29 -0700

    add second solution using Counter() method

--- Problems/242. Valid Anagram/valid_anagram.py
+++ Problems/242. Valid Anagram/valid_anagram.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s, t):
         if len(s) < len(t): # first check if s has enough letter to even create an anagram for t
@@ -9,3 +11,13 @@ class Solution:
             # if the count is not the same then an anagram cannot be created
                 return False
         return True
+    
+
+    def isAnagramTwo(self, s, t):
+        return Counter(s) == Counter(t)
+        # - the Counter() function from collections library creates a dictionary with the element as the key and the number of times
+        #   that element occurs as the value
+        # ----- Example -----
+        # isAnagramTwo(s = "anagram", t = "nagaram")
+        # - Counter(s) will return Counter({'a':3, 'n':1, 'g':1, 'r':1,'m':1})
+        # - Counter(t) will also return Counter({'a':3, 'n':1, 'g':1, 'r':1,'m':1})
