e788ccf6d2f63626f55ff06fe02be69d14bc61fc
Collin Sakuma <collinmsakuma@gmail.com>
2023-07-06 14:31:47 -0700

    loop through range and use divmod

---
+++ Problems/989. Add to Array-Form of Integer/add_to_array_form_integer.py
@@ -0,0 +1,14 @@
+def addToArrayForm(num, k): # solution works. It runs too slow
+    num_string = ""
+    for i in num:
+        num_string += str(i)
+    num_string = int(num_string) + k
+    return [int(i) for i in str(num_string)]
+
+def addToArrayFormTwo(self, num, k):
+    for i in range(len(num) - 1, -1, -1):
+        k, num[i] = divmod(num[i] + k, 10)
+    while k:
+        k, a = divmod(k, 10)
+        num = [a] + num
+    return num
