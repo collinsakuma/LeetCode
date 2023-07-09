3fcaf037c91f3e260947a580aeca227828201052
Collin Sakuma <collinmsakuma@gmail.com>
2023-07-09 16:12:04 -0700

    second solution added loop though words instead of range(len(words) cleaner and faster solution

--- Problems/2108. Find First Palindromic String in Array/find_palindrom.py
+++ Problems/2108. Find First Palindromic String in Array/find_palindrom.py
@@ -7,3 +7,9 @@ class Solution:
                 return words[i]
         return ''
         # if no words are palandromic return an empty string
+
+    def firstPalindromeTwo(self, words): # solution from 7/9/23
+        for word in words: # loop though words list instead of loop though range of len(words) much cleaner this way
+            if word == word[::-1]: # check if word == to backwards version of word if true than word is a palindrom
+                return word
+        return "" # return empty string if no word in words is a palindrom
