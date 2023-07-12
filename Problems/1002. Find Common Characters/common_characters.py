29f165f17b67bfd144aa1a34b62b0b951f4e57d1
Collin Sakuma <collinmsakuma@gmail.com>
2023-07-12 13:53:51 -0700

    find of set of unique letters in the first word, then find the min amound of times each letter appears in all words

---
+++ Problems/1002. Find Common Characters/common_characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def commonChars(self, words):
+        common = [] # create empty list to hold common letters
+        word1 = set(words[0]) # create a set of unique letters in the first word
+
+        for char in word1: # loop though the set of letters
+            frequency = min([word.count(char) for word in words]) # find the minimum frequency of how many times each letter occurs in all words
+            common += [char] * frequency # add the letter to the common list * the amount of times the letter appears in all words
+
+        return common 
