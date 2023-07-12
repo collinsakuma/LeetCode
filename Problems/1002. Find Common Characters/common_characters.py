80ad4c39330c48f679061b5126eaf828a1bc5b99
Collin Sakuma <collinmsakuma@gmail.com>
2023-07-12 13:55:06 -0700

    add not for how the frequency selects the min value

--- Problems/1002. Find Common Characters/common_characters.py
+++ Problems/1002. Find Common Characters/common_characters.py
@@ -5,6 +5,7 @@ class Solution:
 
         for char in word1: # loop though the set of letters
             frequency = min([word.count(char) for word in words]) # find the minimum frequency of how many times each letter occurs in all words
+            # if the char does not appear in one of the words 0 will be set as the frequency beacuse if gets the min of all words. 
             common += [char] * frequency # add the letter to the common list * the amount of times the letter appears in all words
 
         return common 
