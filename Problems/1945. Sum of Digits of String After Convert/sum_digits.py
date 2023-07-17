ef263be4aaf367f2909819167a1f9de68dc067d1
Collin Sakuma <collinmsakuma@gmail.com>
2023-07-17 14:04:22 -0700

    use ord((letter)-96) to conver letters to their number equivalent use while loop to perform necessary transformations k number of times

--- Problems/1945. Sum of Digits of String After Convert/sum_digits.py
+++ Problems/1945. Sum of Digits of String After Convert/sum_digits.py
@@ -1,14 +1,12 @@
 class Solution:
     def getLucky(self, s, k):
-        new_s = ""
-
-        for letter in s:
-            new_s += str(ord(letter) - 96)
-        while k != 0:
-            transform = 0
-            for num in str(new_s):
-                transform += int(num)
-            new_s = transform
-            k -= 1
-        return new_s
-    
+        new_s = "" # variable to convert the letters into a string with the number values
+        for letter in s: # itterate thought the string
+            new_s += str(ord(letter) - 96) # conver each letter to its number complement and add it to the new_s string
+        while k != 0: # k denotes the times the operation needs to be performed so continue the loop while k does not equal 0
+            transform = 0 # transform value to sum numbers in string
+            for num in str(new_s): # loop trough the new_s 
+                transform += int(num) # add each digit in new_s to the transform variable as an integer
+            new_s = transform # set transform as the new value of new_s
+            k -= 1 # reduce the number of times k needs to be iterated by 1 as one iteration has been completed. 
+        return new_s # if while loop is exited return the value of new_s
