16dbbdfe07b78954290b4c660748b7bb40e4ce76
Collin Sakuma <collinmsakuma@gmail.com>
2023-08-07 17:10:15 -0700

    use an empty dict to keep track of each letter and how many times it has occured in the string

---
+++ Problems/2351. First Letter to Appear Twice/first_to_appear_twice.py
@@ -0,0 +1,12 @@
+class Solution:
+    def repeatedCharacter(self, s):
+        dict = {} # create an empty dictionary to keep track of each letter as the key and how many times it has occured as the value
+        
+        for i in s: # loop though each letter in the string s
+            if i not in dict: # if the letter is not already a key in the dict create a new key value pair with i as the key and 1 as the value
+                dict[i] = 1
+            else: # if i already a key in dict increment its value by 1
+                dict[i] += 1
+            
+            if dict[i] == 2: # check if i has a value of 2 meaning that it has appeared twice in the string. If true return that letter
+                return i
