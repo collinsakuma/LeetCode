fff474d8d2476883e28d4635803a1227cbd49371
Collin Sakuma <collinmsakuma@gmail.com>
2023-08-13 14:56:24 -0700

    loop though nums check if index at num-1 is negative if negative number has been encountered before in array, else turn num at index num-1 negative

---
+++ Problems/442. Find All Duplicates in an Array/find_dups_in_array.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    # idea is to loop though list and for each number check if the number at that index is negative (meaning that it has been encounter before in the list and turned to a negative value)
+    def findDuplicates(self, nums):
+        answer = [] # empty list to add numbers that appear twice in the list
+        for num in nums: # loop through list
+            num = abs(num) # set num to the absolute value of num ( positive number )
+            if nums[num - 1] < 0: # check if the value at nums[num - 1] is less than 0
+                answer.append(num) # if value is negative append that number to answer
+            else:
+                nums[num - 1] = - nums[num - 1] # if value is not negative make the number at index num-1 negative
+        return answer
