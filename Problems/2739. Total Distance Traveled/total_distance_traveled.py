25f2aead7972023cd767c0e1bae4e7e0a62de6fc
Collin Sakuma <collinmsakuma@gmail.com>
2023-08-14 14:49:22 -0700

    second solution reduces main tank by as many 5 liter increments as possible faster version of original answer

--- Problems/2739. Total Distance Traveled/total_distance_traveled.py
+++ Problems/2739. Total Distance Traveled/total_distance_traveled.py
@@ -1,4 +1,5 @@
 class Solution:
+    # first solution only reduces by increments of 5 liters at a time
     def distanceTraveled(self, mainTank, additionalTank):
         distance = 0 # set variable to keep track of distance traveled
         while mainTank != 0: # while there is still gas in mainTank continue the loop
@@ -10,5 +11,20 @@ class Solution:
                     additionalTank -= 1
             else: # if there is less than 5 liters left in the mainTank finish off the remaining fuel and add the remaining distance traveled
                 distance += mainTank * 10
+                mainTank -= mainTank
+        return distance
+    
+    # second solution will reduce main tank by as many increments of 5 as possible at one time
+    def distanceTraveledTwo(self, mainTank, additionalTank):
+        distance = 0
+        while mainTank != 0:
+            if mainTank >= 5:
+                used = mainTank // 5 # amount of 5 liter chunks that can be used at one time
+                distance += (used * 5) * 10 
+                mainTank -= (used * 5)
+                mainTank += min(used, additionalTank) # add fuel from the additional fuel tank take the min number between the amount of 5 liter increments of gas that was used of the amount of additional liters of gas
+                additionalTank -= min(used, additionalTank)
+            else:
+                distance += 10 * mainTank
                 mainTank -= mainTank
         return distance
