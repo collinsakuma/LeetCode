c73d6a9ffa29fe12975b845444bf7eb1560dabf2
Collin Sakuma <collinmsakuma@gmail.com>
2023-08-25 12:26:21 -0700

    second solution using geneartes to cut down on the amount memory used, entire string is not stored in memory at once

--- Problems/1662. Check if Two String Arrays are Equivalent/array_strings_equal.py
+++ Problems/1662. Check if Two String Arrays are Equivalent/array_strings_equal.py
@@ -3,4 +3,23 @@ class Solution:
         return "".join(word1) == "".join(word2)
     
 
+
+# second solution Using Generators and Yield Statements #
+
+# - Generators are a kind of iterators which do not store all the values in memory, 
+#   they simply generate the values on the fly (thus you cannot go back and perform 
+#   operations on previously seen indices).
+
+    def arrayStringsAreEqualTwo(self, word1, word2):
+        for wordOne, wordTwo in zip(self.generate(word1), self.generate(word2)):
+            if wordOne != wordTwo:
+                return False
+        return True
     
+    def generate(self, wordlist):
+        for word in wordlist:
+            for char in word:
+                yield char
+        yield None # yield None signifies that the given input has been exhausted
+    
+
