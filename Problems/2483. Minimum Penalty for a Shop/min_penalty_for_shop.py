982a91771941308799bddbc226024de97f1da3f4
Collin Sakuma <collinmsakuma@gmail.com>
2023-08-29 13:55:20 -0700

    single pass solution, increase and decrease score as the string is looped through, set new high score as it is found

--- Problems/2483. Minimum Penalty for a Shop/min_penalty_for_shop.py
+++ Problems/2483. Minimum Penalty for a Shop/min_penalty_for_shop.py
@@ -1,6 +1,7 @@
 import math
 
 class Solution:
+    # first solution code works but runs too slow
     def bestClosingTime(self, customers):
         # set penalty variable initially to infinity as it will be replace by the smallest possible penalty
         penalty, penalty_hour = math.inf, 0
@@ -22,3 +23,21 @@ class Solution:
             hour += 1
         
         return penalty_hour # return the hour that the store should close to incure the smallest penalty
+    
+    
+    # second single pass solution
+    def bestClosingTime(self, customers):
+        max_score, score = 0, 0 
+        hour = -1
+
+        for i, c in enumerate(customers): # loop through customers string while keeping track of the index at i
+            # at each index check if it is a "Y" 
+            if c == "Y": # if its a "Y" increase the score by 1
+                score += 1
+            else: # else decrease the score by 1
+                score -= 1
+            # while looping through customers check if the current max_score is greater than current score
+            if score > max_score:
+                # if score ever exceed max_score set a new max_score and hour to close the store at
+                max_score, hour = score, i
+        return hour + 1
