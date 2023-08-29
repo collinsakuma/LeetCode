790f9f7eda4a1dba9ec1d212c94dde7f594f2335
Collin Sakuma <collinmsakuma@gmail.com>
2023-08-29 14:15:38 -0700

    loop through range of shorter word, add alternating letters from each word, return the new string plus either words leftover characters

--- Problems/1768. Merge Strings Alternately/merge_strings_alternately.py
+++ Problems/1768. Merge Strings Alternately/merge_strings_alternately.py
@@ -11,3 +11,12 @@ class Solution:
             if word2: # if word2 still has elements
                 output += word2.pop(0) # pop first element from word2 and add it to output
         return output
+    
+    # second faster running solution
+    def mergeAlternatelyTwo(self, word1, word2):
+        output = ""
+        
+        for i in range(min(len(word1), len(word2))): # loop through a range of either length of word1 or word2 which ever has the shorter length
+            output += word1[i] + word2[i] # add the index i from word1 then from word2
+
+        return output + word1[i + 1:] + word2[i + 1:] # return the index + the leftover characters, if any, from word1 or word2
