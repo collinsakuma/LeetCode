f3e5e3c38fd2dcf3710cf771d7c5178ab6de2a81
Collin Sakuma <collinmsakuma@gmail.com>
2023-09-15 13:08:23 -0700

    use stack approach checking conditions of each asteroid in asteroids, adding and removing from output as loop operates

--- Problems/735. Asteroid Collision/asteroid_collision.py
+++ Problems/735. Asteroid Collision/asteroid_collision.py
@@ -13,3 +13,26 @@ class Solution:
             else:
                 stack.append(a)
         return stack
+    
+    def asteroidCollisionTwo(self, asteroids):
+        output = [] # set list to stack and pop asteroids in and out of
+
+        for i in asteroids: # loop through asteroids
+            # check several conditions 
+            # 1. output list exist and is not empty
+            # 2. the last element in output is greater than 0
+            # 3. the current i in the loop is less than 0 ( negative number ) 
+            while output and output[-1] > 0 and i < 0:
+                if output[-1] + i < 0:
+                    # if ouput[-1] and i combined is negative then the two asteroids have collided and i is bigger than output[-1]
+                    output.pop() # pop the last asteroid from output
+                elif output[-1] + i > 0:
+                    # if output[-1] and i combined is greater than 0 then output[-1] was larger than i, do not modify output
+                    break
+                else:
+                    output.pop()
+                    break
+            else:
+                # if output is empty, the current last element is negative, or i is positive
+                output.append(i) # append i to output
+        return output
