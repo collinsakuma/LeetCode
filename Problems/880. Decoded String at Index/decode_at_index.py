a48494835e9ba1ab4ae0226493a6939d591f052c
Collin Sakuma <collinmsakuma@gmail.com>
2023-09-27 13:12:11 -0700

    find max length of decoded string, then traverse backwards to find kth letter:

--- Problems/880. Decoded String at Index/decode_at_index.py
+++ Problems/880. Decoded String at Index/decode_at_index.py
@@ -15,3 +15,27 @@ class Solution:
             else:
                 tape += i
         return tape[k - 1] # return the kth letter in the tape, - 1 to account for 0 index
+    
+    def decodeAtIndexTwo(self, s, k):
+        decoded_length = 0 # length of decoded string
+
+        for char in s:
+            if char.isdigit(): # if character is a number update decoded_length
+                decoded_length *= int(char)
+            else:
+                decoded_length += 1 # if character is a letter increment decoded_length
+        
+        # loop through input string in reverse order to find the kth character
+        for i in range(len(s) - 1, -1, -1):
+            current_char = s[i]
+
+            if current_char.isdigit():
+                # if character is a number adjust decoded_length and k
+                decoded_length //= int(current_char)
+                k %= decoded_length
+            else:
+                # if character is a letter check if it is the kth character
+                if k == 0 or decoded_length == k:
+                    return current_char
+                decoded_length -= 1
+        return ""
