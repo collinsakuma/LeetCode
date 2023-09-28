1d4dc70854eb67f75c767b94ffe00c2ed7929cab
Collin Sakuma <collinmsakuma@gmail.com>
2023-09-28 12:51:36 -0700

    two pointer method, loop though array from the front and back checking for even and odd numbers and swapping them when necessary

--- Problems/905. Sort Array By Parity/sort_array_by_parity.py
+++ Problems/905. Sort Array By Parity/sort_array_by_parity.py
@@ -14,3 +14,20 @@ class Solution:
                 odds.append(nums[i])
         final = final + evens + odds # populate the final array add evens array and then odds array to final
         return final
+    
+
+    # solution using two pointer method
+    def sortArrayByParityTwo(self, nums):
+        i, j = 0, len(nums) - 1 # set to pointer to travers list from front and back
+
+        while i < j: # loop while i and j pointers have not converged in the middle
+            while i < j and nums[i] % 2 == 0: #if number at index i is an even number increment i
+                # ( increment i because even numbers stay at the front of the list )
+                i += 1
+            while i < j and nums[j] % 2 == 1: # if number at index j is an odd number increment j lower
+                # ( increment j because even number go to the end of the list as j is the back half of the list leave the odd number at index j)
+                j -= 1
+            # if nums[i] is an odd number and nums[j] is an even number swap the two vales so the even number 
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        return nums
