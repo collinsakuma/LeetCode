db4d2b686241f813c59e633f7be7ee862ffcd3a4
Collin Sakuma <collinmsakuma@gmail.com>
2023-10-04 21:52:31 -0700

    decrpty string solution

---
+++ Problems/1309. Decrpyt String from Alphabet to Integer Mapping/decript_strin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        output = ""
+        i = 0
+        while i < len(s):
+            if i + 2 < len(s) and s[i + 2] == "#":
+                c = chr(int(s[i:i + 2]) + ord("a") - 1)
+                i += 3
+            else:
+                c = chr(int(s[i]) + ord("a") - 1)
+                i += 1
+            output += c
+        return output
