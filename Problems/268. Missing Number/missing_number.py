3b9df62efe2c9ca6b52e9a3a5d1c91758a113861
Collin Sakuma <collinmsakuma@gmail.com>
2023-10-11 12:27:25 -0700

    math based solution, take sum of range plus 1 to find the assumed total find difference between that and sum of nums to find missing number

--- Problems/268. Missing Number/missing_number.py
+++ Problems/268. Missing Number/missing_number.py
@@ -5,3 +5,10 @@ class Solution:
             if i not in nums: # check for the missing numbers
                 # when missing number is found return the number
                 return i
+            
+    def missingNumberTwo(self, nums):
+        # only one number is missing from the list nums
+        # take the sum of the range of len nums + 1
+        # take the sum of the nums given
+        # the difference between the two is the missing number from the list nums
+        return sum(range(len(nums) + 1)) - sum(nums)
