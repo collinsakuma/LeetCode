7851052e04ea415252d1eb49d24c3a4e88968990
Collin Sakuma <collinmsakuma@gmail.com>
2023-10-22 22:15:08 -0700

    check if num is in arr then check if the count of num is equal in both arrays

---
+++ Problems/1460. Make Two Arrays Equal by Reversing Subarrays/make_two_equal_subarrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canBeEqual(self, target, arr):
+        for num in target: # loop though all numbers in target array
+            if num not in arr: # if num not in arr then both arrays cannot become the same
+                return False
+            if target.count(num) != arr.count(num): # next check if the target array and arr have the same count of num
+                return False
+        # if all num pass checks arrays can be made equal return True
+        return True
