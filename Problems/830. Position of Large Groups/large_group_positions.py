8c5d4a2ce4d4791ce4d7d1d0d9115a48a431d492
Collin Sakuma <collinmsakuma@gmail.com>
2023-11-28 21:19:01 -0800

    solution

---
+++ Problems/830. Position of Large Groups/large_group_positions.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largeGroupPositions(self, s):
+        left = 0
+        return_list = []
+        s += 1
+        for index, letter in enumerate(s):
+            if letter != s[left]:
+                if index - left >= 3:
+                    return_list.append([left, index - 1])
+                left = index
+        
+        return return_list
