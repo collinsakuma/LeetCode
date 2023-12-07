d4bf7255b150b69ac7fc9ad8fb04f55a60962a23
Collin Sakuma <collinmsakuma@gmail.com>
2023-12-06 21:44:10 -0800

    solution using array

--- Problems/1903. Largest Odd Number in String/largest_odd_num_in_string.py
+++ Problems/1903. Largest Odd Number in String/largest_odd_num_in_string.py
@@ -15,3 +15,11 @@ class Solution:
             if num[i] in {'1', '3', '5', '7', '9'}: # if num in dictionary of odd numbers
                 return num[:i + 1] # return number up to i + 1
         return "" 
+    
+    # third solution using a array instead of a dictionary
+    def largestOddNumber(self, num: str) -> str:
+        odds = ["1", "3", "5", "7", "9"]
+        for i in range(len(num) - 1, -1, -1):
+            if num[i] in odds:
+                return num[:i + 1]
+        return ""
