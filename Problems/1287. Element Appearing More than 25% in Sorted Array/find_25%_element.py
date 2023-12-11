dadc3a77e106dca7b6b6e0cac51e5e8350f25ec4
Collin Sakuma <collinmsakuma@gmail.com>
2023-12-11 12:58:27 -0800

    use Counter() to find the .most_common() element in the list

--- Problems/1287. Element Appearing More than 25% in Sorted Array/find_25%_element.py
+++ Problems/1287. Element Appearing More than 25% in Sorted Array/find_25%_element.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     def findSpecialInteger(self, arr):
         num_required = len(arr) / 4 # find 25% of the length of the array
@@ -6,3 +8,8 @@ class Solution:
             # if num apprears more than 25% of len(arr) return that number
             if arr.count(num) > num_required:
                 return num
+            
+    def findSpecialIntegerTwo(self, arr):
+        count = Counter(arr) # use counter to create a dictionary of each element as the key and its frequency as the value
+        # return the most common 1 in count, item that appears the most frequently, [0] first object in dict, [0] first element in the object
+        return count.most_common(1)[0][0]
