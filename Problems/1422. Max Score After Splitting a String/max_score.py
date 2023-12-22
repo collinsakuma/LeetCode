c283a7dcc536334647dadec7d726b1656d99ead0
Collin Sakuma <collinmsakuma@gmail.com>
2023-12-22 15:46:54 -0800

    second attempt use max() to find if new highest score has been achieved

--- Problems/1422. Max Score After Splitting a String/max_score.py
+++ Problems/1422. Max Score After Splitting a String/max_score.py
@@ -11,3 +11,15 @@ class Solution:
                 score = left.count("0") + right.count("1") # if greater than score, set new score
 
         return score
+    
+
+    # second attempt from scratch 12/22/23 same general process, use max() method instead of if statement to find if there is a new highest score
+    def maxScoreTwo(self, s):
+        max_score = 0
+
+        for i in range(1, len(s)):
+            left = s[:i]
+            right =s[i:]
+            max_score = max(max_score, left.count("0") + right.count("1"))
+        
+        return max_score
