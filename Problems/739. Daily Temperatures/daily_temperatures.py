4ee5a9c990aca8e4c98ca3ffecf3ce4003894d04
Collin Sakuma <collinmsakuma@gmail.com>
2024-01-31 11:53:37 -0800

    second solution, Exceeds TLE, but otherwise works as a solution, double for loop

--- Problems/739. Daily Temperatures/daily_temperatures.py
+++ Problems/739. Daily Temperatures/daily_temperatures.py
@@ -10,3 +10,21 @@ class Solution:
                 output[index] = i - index # check how many indicies have been crossed since last lesser temperature
             stack.append([i, j])
         return output 
+    
+
+    # solution created on 1/31/24 will work on shorter test cases but TLE on longer ones. 
+    def dailyTemperaturesTwo(self, temperatures):
+        output = []
+
+        for i in range(len(temperatures)):
+            flag = False
+
+            for j in range(i, len(temperatures)):
+                if temperatures[j] > temperatures[i]:
+                    output.append(j - i)
+                    flag = True
+                    break
+                if not flag:
+                    output.append(0)
+
+        return output
