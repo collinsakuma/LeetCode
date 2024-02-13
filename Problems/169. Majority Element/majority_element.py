0fba4dc80a52821cac00ee3a805185f2582cd6ca
Collin Sakuma <collinmsakuma@gmail.com>
2024-02-12 20:02:46 -0800

    use Counter, one line solution

--- Problems/169. Majority Element/majority_element.py
+++ Problems/169. Majority Element/majority_element.py
@@ -1,6 +1,11 @@
+from collections import Counter
+
 class Solution:
     def majorityElement(self, nums):
         for num in set(nums): # loop though a set of unique numbers in nums
             # count how many times num is in numbers, if it occures more than the length of nums / 2 return that number
             if nums.count(num) > len(nums) / 2:
                 return num
+            
+    def majorityElementTwo(self, nums):
+        return sorted(Counter(nums).items(), key=lambda item: item[1], reverse=True)[0][0]
