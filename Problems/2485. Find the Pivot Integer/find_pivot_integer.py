4095e01f13b2e968345cee47fa3d12b0e4b9fcca
Collin Sakuma <collinmsakuma@gmail.com>
2024-03-13 12:53:10 -0700

    second solution 3/13 create list of numbers from 1 to n loop though range to find pivot point

--- Problems/2485. Find the Pivot Integer/find_pivot_integer.py
+++ Problems/2485. Find the Pivot Integer/find_pivot_integer.py
@@ -9,3 +9,22 @@ class Solution:
                 return i
             left += i # if pivot number not found reduce left by i for next iteration
         return -1
+    
+    def pivotIntegerTwo(self, n):
+        # n equals 1 is an edge case where 1 == 1 
+        if n == 1:
+            return 1
+        # create an empty list to hold the numbers 1 - n
+        num_list = []
+
+        # fill out num_list with numbers 1 to n
+        for i in range(1, n + 1):
+            num_list.append(i)
+
+        # loop through range of length of n - 1
+        for j in range(len(num_list)):
+            # if index j splits the two sides of num_list evenly then the pivot point has been found
+            if sum(num_list[:j + 1]) == sum(num_list[j:]):
+                return j + 1
+            
+        return -1 # return -1 if no pivot point is found 
