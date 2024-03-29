e5e86cd892e2b9dd87bfb0db0e4a5143ee61b812
Collin Sakuma <collinmsakuma@gmail.com>
2024-03-28 20:48:17 -0700

    use a sliding window and a dictionary to keep track of if any element exceeds k numbers of occurances in the subarray

---
+++ Problems/2958. Length of Longest Subarray With at Most K Frequency/max_subarray_length.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubarrayLength(self, nums, k):
+        frequency = defautdict(int)
+        n = len(nums)
+
+        i, j, output = 0, 0, 1
+
+        while i < n and j < n:
+            # while the element on the right side of the window appears more than k times
+            while frequency[nums[j]] > k:
+                # remove the left most element at index i and increment the sliding window
+                frequency[nums[i]] -= 1
+                i += 1
+            # after each movement of the window check if there is a new longest subarray 
+            output = max(output, j - 1 + 1)
+            # expand the right side of the sliding window right
+            j += 1
+        # return the longest subarray length
+        return output
