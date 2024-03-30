cf67e44d3c4b623ec3980b78d3b7e2f108bd4315
Collin Sakuma <collinmsakuma@gmail.com>
2024-03-29 21:59:51 -0700

    correct code, add dict update to begining after initial while loop

--- Problems/2958. Length of Longest Subarray With at Most K Frequency/max_subarray_length.py
+++ Problems/2958. Length of Longest Subarray With at Most K Frequency/max_subarray_length.py
@@ -8,6 +8,8 @@ class Solution:
         i, j, output = 0, 0, 1
 
         while i < n and j < n:
+            # incresae the count of element at index j to the dict
+            frequency[nums[j]] += 1
             # while the element on the right side of the window appears more than k times
             while frequency[nums[j]] > k:
                 # remove the left most element at index i and increment the sliding window
