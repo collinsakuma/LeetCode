d27bcf10b33a54550257b5808efdfd44718f286a
Collin Sakuma <collinmsakuma@gmail.com>
2024-04-01 11:11:52 -0700

    split string into a list, use .pop() to pinpoint the last word in the list, return its length

--- Problems/58. Length of Last Word/python/length_of_last_word.py
+++ Problems/58. Length of Last Word/python/length_of_last_word.py
@@ -4,3 +4,17 @@ class Solution:
         # split the string into a list between the spaces of the words
         return len(split_string[-1])
         # return the length of the last element in the list
+    
+    # solution 4/1/24
+    def lengthOfLastWordTwo(self, s):
+        s = s.split() # split the string 
+        while s: # while there are elements in s
+            word = s.pop() # pop the last element from the list s
+            if word:
+                return len(word) # return the length of the word
+            
+
+    # iteration of second solution cleaned up but same general method
+    def lengthOfLastWordThree(self, s):
+        s = s.split()
+        return len(s.pop())
