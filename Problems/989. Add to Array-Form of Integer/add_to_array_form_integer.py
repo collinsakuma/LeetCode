c32f1ac4b5d550f3ee61975f0f610a6184d895dd
Collin Sakuma <collinmsakuma@gmail.com>
2024-04-02 14:29:17 -0700

    added some notes for divmode() method

--- Problems/989. Add to Array-Form of Integer/add_to_array_form_integer.py
+++ Problems/989. Add to Array-Form of Integer/add_to_array_form_integer.py
@@ -5,6 +5,10 @@ def addToArrayForm(num, k): # solution works. It runs too slow
     num_string = int(num_string) + k
     return [int(i) for i in str(num_string)]
 
+
+# divmod(divided, divisor)
+# divided - The number you want to divide
+# divisor - The number you want to divide with
 def addToArrayFormTwo(self, num, k):
     for i in range(len(num) - 1, -1, -1):
         k, num[i] = divmod(num[i] + k, 10)
