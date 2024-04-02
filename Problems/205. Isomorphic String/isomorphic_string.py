1a32a70c365b4daf7830185f90955c39af5bd78b
Collin Sakuma <collinmsakuma@gmail.com>
2024-04-02 12:49:03 -0700

    use a dictionary to pair the letters in each string

---
+++ Problems/205. Isomorphic String/isomorphic_string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        # dict to hold letter and letter that it will be switched to
+        mirror_dict = {}
+
+        # if there is a difference in the unique characters in the strings
+        # they cannot be isomorphic
+        if len(set(s)) != len(set(t)):
+            return False
+        
+        # loop though a range of the length of the string
+        for i in range(len(s)):
+            # if the letter at s[i] is already in the dict ( mirrored to another letter )
+            if s[i] in mirror_dict:
+                # if the letter t[i] doesnt match the letter s[i] is already beeing swapped with, return False
+                if mirror_dict[s[i]] != t[i]:
+                    return False
+            # if s[i] not paired already with a letter in string t to replace, pair it with t[i] in mirror_dict
+            else:
+                mirror_dict[s[i]] = t[i]
+
+        return True
