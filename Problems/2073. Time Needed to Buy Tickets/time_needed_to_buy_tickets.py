711f639c178943ab9657779b363f35c33d065ea8
Collin Sakuma <collinmsakuma@gmail.com>
2024-04-09 12:18:28 -0700

    O(n) solution loop though only once handle person k and the people before differenclty than those that come after in the Q

--- Problems/2073. Time Needed to Buy Tickets/time_needed_to_buy_tickets.py
+++ Problems/2073. Time Needed to Buy Tickets/time_needed_to_buy_tickets.py
@@ -31,3 +31,19 @@ class Solution:
                     tickets[index] -= 1
                     seconds += 1
         return seconds
+    
+    def timeRequiredToBuyThree(self, tickets, k):
+        time = 0
+
+        for index, ticket in enumerate(tickets):
+            # for all people infront of person k in the que
+            # - if they have less tickets than person k to buy then they will have bought all of their tickets before person k does
+            # - if they have more than or just as many tickets to buy as person k they they can only buy the same number of tickets as person k
+            if index <= k:
+                time += min(tickets[index], tickets[k])
+            # for people after person k
+            # - they can buy k - 1 tickets because once person k buys their last ticket this person will not beable to buy anymore
+            # - if they are buying less than person k - 1 tickets they can purchase all of them
+            else:
+                time += min(tickets[index], tickets[k] - 1)
+        return time
