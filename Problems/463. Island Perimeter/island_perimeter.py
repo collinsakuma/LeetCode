76dbe1be05d3e52a1931f9ad7f7a49ac2d940dfe
Collin Sakuma <collinmsakuma@gmail.com>
2024-04-18 13:30:16 -0700

    dfs solution

--- Problems/463. Island Perimeter/island_perimeter.py
+++ Problems/463. Island Perimeter/island_perimeter.py
@@ -22,3 +22,38 @@ class Solution:
                         perimeter -= 2
         
         return perimeter
+    
+
+    #### Second Solution using DFS / Recursion ####
+    def islandPerimeterTwo(self, grid):
+        columns = len(grid[0])
+        rows = len(grid)
+        perimeter = 0
+
+        def dfs(row, column):
+            # check if the adjacent sides are valid grid locations
+            # if not valid locations then a side is garunteed to be there as no intersection land can bee in that position
+            if row < 0 or row >= rows or column < 0 or column >= columns or grid[row][column] == 0:
+                # return that there is a vaid side
+                return 1
+            # if the gid position equals -1 that this is an already accounted for gird spot, return 0
+            if grid[row][column] == -1:
+                return 0
+            # mark the current grid spot as already visited
+            grid[row][column] = -1
+            
+            # check the 4 adjacent sides 
+            return (dfs(row + 1, column) +
+                    dfs(row - 1, column) +
+                    dfs(row, column + 1) +
+                    dfs(row, column - 1))
+        
+        # loop though grid positions
+        for row in range(rows):
+            for column in range(columns):
+                # if the grid position is valid, run the dfs() function to check its adjacent positions
+                if grid[row][column] == 1:
+                    # add the number of valid sides that is returned
+                    perimeter += dfs(row, column)
+
+        return perimeter
