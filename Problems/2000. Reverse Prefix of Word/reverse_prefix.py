cdc501461eacf98a467453cd15027f3d61229762
Collin Sakuma <collinmsakuma@gmail.com>
2024-05-01 21:44:36 -0700

    enumerate through word find the target character, flip the prefix and add the rest of the word

--- Problems/2000. Reverse Prefix of Word/reverse_prefix.py
+++ Problems/2000. Reverse Prefix of Word/reverse_prefix.py
@@ -4,3 +4,12 @@ class Solution:
         return (word[index + 1][::-1] + word[index + 1:])
         # word[index + 1][::-1] takes the prefix (letter up to and including ch) and reverses them
         # word[index + 1:] adds the remaining letters in word after index of ch in word
+    
+    def reversePrefixTwo(self, word, ch):
+        # loop through using enumerate() to track index as well
+        for index, letter in enumerate(word):
+            # if we encounter the target index
+            if letter == ch:
+                # reverse the prefix and add the rest of the word to the string
+                return word[index::-1] + word[index + 1:]
+        return word
