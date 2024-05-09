5c003d2914908e5441f886249fb40561a1ce4190
Collin Sakuma <collinmsakuma@gmail.com>
2024-05-08 21:39:41 -0700

    use a heap

--- Problems/506. Relative Ranks/relative_ranks.py
+++ Problems/506. Relative Ranks/relative_ranks.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Solution:
     def findRelativeRanks(self, score):
         # create a copy of scores sorted in reverse order
@@ -16,3 +18,35 @@ class Solution:
                 place.append(str(current_place + 1))
         
         return place # return list of places
+    
+    def findRelativeRanksTwo(self, score):
+        n = len(score)
+
+        heap = []
+
+        # add all the scores to a heap with their respective positional index in score array
+        for index, score in enumerate(score):
+            heapq.heappush(heap, (-score, index))
+
+        # initalize a new array of length n
+        ranks = [0] * n
+        # start the places at 1st place
+        place = 1
+
+        # while heap continue to loop
+        while heap:
+            # set the positional index of the score
+            starting_index = heapq.heappop(heap)[1]
+            # if place is 1 through 3 give that person the medal
+            if place == 1:
+                ranks[starting_index] = 'Gold Medal'
+            elif place == 2:
+                ranks[starting_index] = 'Silver Medal'
+            elif place == 3:
+                ranks[starting_index] = 'Bronze Medal'
+            # else set their position in string form
+            else:
+                ranks[starting_index] = str(place)
+            place += 1
+        
+        return ranks
