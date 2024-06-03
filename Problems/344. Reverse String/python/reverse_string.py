03129ef3fcf600f8d0c1baf85cadac53d3169eb9
Collin Sakuma <collinmsakuma@gmail.com>
2024-06-02 22:28:38 -0700

    new solution two pointer

--- Problems/344. Reverse String/python/reverse_string.py
+++ Problems/344. Reverse String/python/reverse_string.py
@@ -6,3 +6,13 @@ class Solution:
         # - can not use s = s[::-1]
         # - must use s[:] becuase s[:] points to the actual elements in the list and modifies each one
         s[:] = s[::-1]
+
+    def reverseStringThree(self, s):
+        front, back = 0, len(s) - 1
+
+        while front < len(s) // 2:
+            s[front], s[back] = s[back], s[front]
+            front += 1
+            back += 1
+
+            
