68b1d79364313a125ae424e6c9305cab4f12c27d
Collin Sakuma <collinmsakuma@gmail.com>
2024-06-05 13:49:41 -0700

    second solution using a set and min words

--- Problems/1002. Find Common Characters/common_characters.py
+++ Problems/1002. Find Common Characters/common_characters.py
@@ -9,3 +9,21 @@ class Solution:
             common += [char] * frequency # add the letter to the common list * the amount of times the letter appears in all words
 
         return common 
+    
+    def commonCharsTwo(self, words):
+        # set array to hold common characters
+        common_characters = []
+
+        # loop through a set of letters in the first word
+        for letter in set(words[0]):
+            # set a count of the # of occurances of letter in the first word
+            count = words[0].count(letter)
+
+            # loop through the rest of the words in words
+            for word in words[1:]:
+                # find the minimum count of letter in words
+                count = min(word.count(letter), count)
+            # if letter is a common character among all words add it to the list of common characters
+            common_characters += [letter] * count 
+
+        return common_characters
