f57997bb024622fb7b475b14e98eeca3026ae7b4
Collin Sakuma <collinmsakuma@gmail.com>
2024-06-25 22:02:43 -0700

    use dfs

---
+++ Problems/1038. Binary Search Tree to Greater Sum Tree/bst_to_gst.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def bstToGst(self, root):
+        self.total = 0
+        def dfs(node):
+            if node:
+                dfs(node.right)
+                self.total += node.val
+                node.val = self.total
+                dfs(node.left)
+        dfs(root)
+
+        return root
