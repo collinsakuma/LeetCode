0ac8df939b53748378d23effeea95290701bc33b
Collin Sakuma <collinmsakuma@gmail.com>
2024-07-06 22:18:23 -0700

    track the index and the positive of negative direction the pillow is being passed, reverse the order when the pillow gets to the first or last person in the line

---
+++ Problems/2582. Pass the Pillow/pass_the_pillow.py
@@ -0,0 +1,23 @@
+class Solution:
+    def passThePillow(self, n, time):
+        # set the initial direction to positive
+        direction = 1
+        # start at person 1
+        index = 1
+
+        # while there is still time to pass the pillow continue 
+        while time:
+            # pass the pillow in the direction 
+            index += direction
+
+            # if we have reached the last person, reverse the direction to pass back the other way
+            if index >= n:
+                direction = -1
+            
+            # if we reach the first person again reverse the direction again
+            if i == 1:
+                direction = 1
+
+            time -= 1 # reduce the time after the pillow is passed
+
+        return index # return the index of the last person who has the pillow
