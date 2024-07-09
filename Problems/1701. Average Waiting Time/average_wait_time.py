2e0bc5d8ee3978e215116bc9b4f30c0cde7c636d
Collin Sakuma <collinmsakuma@gmail.com>
2024-07-09 11:59:48 -0700

    track the previous order finish time and the total time customers have to wait

--- Problems/1701. Average Waiting Time/average_wait_time.py
+++ Problems/1701. Average Waiting Time/average_wait_time.py
@@ -10,3 +10,28 @@ class Solution:
                 time += j # increase time by the time it take to make the next persons meal, essentailly queing them 
             wait.append(time - i) # append the total wait time for each customer to the wait list
         return sum(wait) / len(wait) # return the average wait time sum and divide by the number of customers
+    
+    def averageWaitingTimeTwo(self, customers):
+        previous_order_finish_time = total_time = 0
+
+        for arrive, order_time in customers:
+            # if the previous customers order is already completed by the 
+            # arrival time of the next customer
+            if arrive >= previous_order_finish_time:
+                # incrase the total time by the order_time ( no additional wait time )
+                total_time += order_time
+                # set a new previous order finish time for
+                # when the current customers order has been finished
+                previous_order_finish_time = arrive + order_time
+
+            else:
+                # - if the previous customers order was not finished when the next
+                #   customer arrived :
+                # - add to the total time the amount of time the next customer waits
+                #   while the cheif is finishing up the previous customers order
+                # - then add the time that it takes to make the next order
+                total_time += (previous_order_finish_time - arrive + order_time)
+                # add the order to set a new previous_order_finish_time
+                previous_order_finish_time += order_time
+        
+        return total_time / len(customers)
