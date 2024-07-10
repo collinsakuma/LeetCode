817d982ec355a08e9ae9cf7ac84f48d4cfaefc52
Collin Sakuma <collinmsakuma@gmail.com>
2024-07-10 14:16:35 -0700

    traverse operations and track the current depth of folder

--- Problems/1598. Crawler Log Folder/crawler_log_folder.py
+++ Problems/1598. Crawler Log Folder/crawler_log_folder.py
@@ -8,3 +8,25 @@ class Solution:
                 if output: # first check if current level isnt main, if main you cannot move up anymore levels
                     output -= 1 # if output is not main level decrease level by 1
         return output # return however many levels deep last folder accessed is
+    
+    def minOperationsTwo(self, logs):
+        depth = 0 # track the current depth of file
+        
+        # loop through operations
+        for operation in logs:
+            # if ../ operation to return to parent folder
+            if '..' in operation:
+                # if depth is already at the root level do nothing as there are no higher folder
+                if depth == 0:
+                    continue
+                # move up a folder and reduce the depth by 1
+                else:
+                    depth -= 1
+            # ./ stay in current file
+            elif '.' in operation:
+                continue
+            # move to a folder in the current folder, increase the depth level
+            else:
+                depth += 1
+        
+        return depth
