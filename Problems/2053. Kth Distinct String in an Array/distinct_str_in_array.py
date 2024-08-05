72d4fadba63d5174e9ba90746e04f7646a66bc11
Collin Sakuma <collinmsakuma@gmail.com>
2024-08-05 08:54:21 -0700

    use Counter() create a list of unique characters return k-1 index if there are more than k distinct characters

--- Problems/2053. Kth Distinct String in an Array/distinct_str_in_array.py
+++ Problems/2053. Kth Distinct String in an Array/distinct_str_in_array.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     def kthDistinct(self, arr, k ):
         output = [] # list of strings that only appear once
@@ -10,3 +12,15 @@ class Solution:
             return ""
         else:
             return output[k - 1] # minus 1 from k to account for the 0 index
+        
+
+    def kthDistinctTwo(self, arr, k):
+        count = Counter(arr) # create a counter dict
+        # use a list comprehension to create a list of letters that have an occurance of only 1
+        distinct_list = [i[0] for i in count.items() if i[1] == 1]
+
+        # if the list of distinct characters is less than k return an empty string
+        if len(distinct_list) < k:
+            return ''
+        else: # else return the kth distinct character
+            return distinct_list[k - 1]
