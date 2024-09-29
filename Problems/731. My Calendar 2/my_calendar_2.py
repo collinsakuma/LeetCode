b9fd4ff0784a819d9ec68d338eb978bae48eea0e
Collin Sakuma <collinmsakuma@gmail.com>
2024-09-28 20:36:52 -0700

    initalize a sorted calendar, add the dates to the calendar

--- Problems/731. My Calendar 2/my_calendar_2.py
+++ Problems/731. My Calendar 2/my_calendar_2.py
@@ -3,18 +3,22 @@ from sortedcontainers import SortedList
 class MyCalendarTwo:
 
     def __init__(self):
+        # create a calendar
         self.calendar = SortedList()
 
     def book(self, start: int, end: int) -> bool:
+        # add the dates to the calendar sorted list
         self.calendar.add((start, 1))
         self.calendar.add((end, -1))
 
-        total = 0
+        total = 0 # set a total
 
+        # loop through the calendar
         for i, j in self.calendar:
+            # add to the total
             total += j
 
-            if total == 3:
+            if total == 3: # if the total is 3 remove the calendar
                 self.calendar.remove((start, 1))
                 self.calendar.remove((end, -1))
                 return False
