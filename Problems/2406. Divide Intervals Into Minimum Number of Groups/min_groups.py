a02669fb2fd682528c0f8c79353e71783875f266
Collin Sakuma <collinmsakuma@gmail.com>
2024-10-13 21:28:50 -0700

    heapq

--- Problems/2406. Divide Intervals Into Minimum Number of Groups/min_groups.py
+++ Problems/2406. Divide Intervals Into Minimum Number of Groups/min_groups.py
@@ -2,10 +2,15 @@ import heapq
 
 class Solution:
     def minGroups(self, intervals):
+        # initalize the heap
         pq = []
 
+        # loop through the intervals
         for left, right in sorted(intervals):
+            # if there are values and the heap and the first one is les than left
             if pq and pq[0] < left:
+                # pop the value from the heap
                 heapq.heappop(pq)
+                # add to the heap
             heapq.heappush(pq, right)
         return len(pq)
