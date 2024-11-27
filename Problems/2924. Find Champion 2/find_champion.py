a2da6fab08bde9ac089926f386ce78950ed57138
Collin Sakuma <collinmsakuma@gmail.com>
2024-11-26 21:57:11 -0800

    solution

---
+++ Problems/2924. Find Champion 2/find_champion.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findChampion(self, n, edges):
+        is_undefeated = [True] * n
+
+        for winner, loser in edges:
+            is_undefeated[loser] = False
+
+        champion = -1
+        champion_count = 0
+
+        for team in range(n):
+            if is_undefeated[team]:
+                champion = team
+                champion_count += 1
+
+        
+        if champion_count == 1:
+            return champion
+        
+        return -1
