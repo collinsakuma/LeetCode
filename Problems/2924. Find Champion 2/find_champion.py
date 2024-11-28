d9a5d60845f1baf591d3414d823f5cc14bf928bf
Collin Sakuma <collinmsakuma@gmail.com>
2024-11-27 20:01:53 -0800

    create dp for teams, loop through the matches and set switch the teams who lose, loop through teams and check if there is more than one champion team

--- Problems/2924. Find Champion 2/find_champion.py
+++ Problems/2924. Find Champion 2/find_champion.py
@@ -1,20 +1,28 @@
 class Solution:
     def findChampion(self, n, edges):
+        # create undefeated dp
         is_undefeated = [True] * n
 
+        # loop through matches
         for winner, loser in edges:
+            # if the team is defeated flip them to False
             is_undefeated[loser] = False
 
+        # set initial campion to -1
         champion = -1
+        # count number of champions
         champion_count = 0
 
+        # loop through teams
         for team in range(n):
+            # if the team never lost set them to champion
             if is_undefeated[team]:
                 champion = team
+                # count the number of champions
                 champion_count += 1
 
-        
+        # if there was only 1 champion return the champion
         if champion_count == 1:
             return champion
         
-        return -1
+        return -1 # return -1 if there are multiple champions
