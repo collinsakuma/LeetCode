34e7eba182f1b77b2289e49901d5467a3f2725b1
Collin Sakuma <collinmsakuma@gmail.com>
2024-12-07 22:12:45 -0800

    sort list

--- Problems/179. Largest Number/largest_number.py
+++ Problems/179. Largest Number/largest_number.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestNumber(self, nums):
         arr = list(map(str, nums))
-
+        # sort the list 
         arr.sort(key=lambda x:x*10, reverse=True)
 
         if arr[0] == '0':
