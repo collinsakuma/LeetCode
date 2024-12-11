310d24c45750d9e7b2dc114745ca67ae6169709f
Collin Sakuma <collinmsakuma@gmail.com>
2024-12-11 14:15:38 -0800

    create a dict of substrings and their frequency in s, return the length of the longest substring that occurs at least three times

---
+++ Problems/2981. Find Longest Special Substring That Occurs Thrice 1/maximum_length.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumLength(self, s):
+        n = len(s)
+        # create a dict to hold substrings and the count of how many times they occur
+        sub_strings = dict()
+
+        # loop through range of n
+        for start in range(n):
+            # loop through range from start to end
+            for end in range(start, n):
+                # check if next letter is the same character
+                # if not add the substring to the dictionary
+                if s[start] != s[end]: break
+                # check if the substring is in the dict, if not create a new entry else increment the sibstrings count
+                if s[start:end + 1] not in sub_strings:
+                    sub_strings[s[start:end + 1]] = 1
+                else:
+                    sub_strings[s[start:end + 1]] += 1
+        
+        # create a list of substring that occur only three or more times
+        sub_strings = [string for string, count in sub_strings.items() if count >= 3]
+
+        # if there are no substrings that occur at least three times return -1
+        # else return the length of the longest substring
+        if not sub_strings:
+            return -1
+        else:
+            return len(max(sub_strings, key=len, default=None))
