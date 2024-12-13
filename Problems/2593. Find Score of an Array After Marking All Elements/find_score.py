ae9c1aa8ec9308680ca777000315891e0233d29a
Collin Sakuma <collinmsakuma@gmail.com>
2024-12-13 15:47:03 -0800

    sort numbers paring them with their original index, mark current elements and its adjacent numbers untill all numbers have been marked

---
+++ Problems/2593. Find Score of an Array After Marking All Elements/find_score.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findScore(self, nums):
+        n = len(nums)
+        sorted_nums = sorted((num, idx) for idx, num in enumerate(nums))
+        score = 0
+
+        for num, idx in sorted_nums:
+            # if number has not been marked add to score
+            if nums[idx] != -1:
+                score += num
+                nums[idx] = -1 # mark current index as seen
+                if idx > 0:
+                    nums[idx - 1] = -1 # mark left neighbor
+                if idx < n - 1:
+                    nums[idx + 1] = -1 # mark right neighbor
+
+        return score
