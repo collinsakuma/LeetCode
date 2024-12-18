0a8a7d8de946c22bf49b561085f6d45a116488a1
Collin Sakuma <collinmsakuma@gmail.com>
2024-12-17 21:30:21 -0800

    add comments, create heap with num and index as tuple, loop through range k replacing num value after applying multiplier

--- Problems/3264. Final Array State After K Multiplication Operations 1/get_final_state.py
+++ Problems/3264. Final Array State After K Multiplication Operations 1/get_final_state.py
@@ -1,11 +1,13 @@
 import heapq
 class Solution:
     def getFinalState(self, nums, k, multiplier):
+        # create heap of num and its index
         heap = [(x, i) for i, x in enumerate(nums)]
         heapq.heapify(heap)
-
+        # loop through range of operations
         for x in range(k):
             x, i = heap[0]
+            # replace heap with new number modified by the multiplier
             heapq.heapreplace(heap, (x * multiplier, i))
 
         for x, i in heap:
