a6c7f79df76f9ee2eadbb6f4fc31243cfbd057d2
Collin Sakuma <collinmsakuma@gmail.com>
2024-12-22 21:59:58 -0800

    tree class

--- Problems/2641. Cousins in Binary Tree 2/replace_value_in_tree.py
+++ Problems/2641. Cousins in Binary Tree 2/replace_value_in_tree.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-class TreeNode:
+class TreeNode: # create binary tree
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
